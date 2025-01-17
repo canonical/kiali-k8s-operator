cdcbaa49a48349d728e509884702de414a3a9598
IbraAoad <Ibrahim.Awwad@canonical.com>
2025-01-17 14:15:09 +0200

    fix reltion names in itests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -77,7 +77,9 @@ async def test_deploy_dependencies(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_add_relation_prometheus(ops_test: OpsTest):
     """Relate the charm_under_test to prometheus."""
-    await ops_test.model.add_relation(APP_NAME, PROMETHEUS_K8S.application_name)
+    await ops_test.model.add_relation(
+        f"{APP_NAME}:prometheus", f"{PROMETHEUS_K8S.application_name}:grafana-source"
+    )
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=60)
 
 
