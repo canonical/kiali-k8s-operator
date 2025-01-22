c1b86ac265c33fc7f918968a889e884468cd9c96
IbraAoad <Ibrahim.Awwad@canonical.com>
2025-01-22 16:07:04 +0200

    add ingress itest

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -12,7 +12,7 @@ import requests
 import yaml
 from pytest_operator.plugin import OpsTest
 
-from tests.integration.helpers import get_k8s_service_clusterip
+from tests.integration.helpers import get_k8s_service_ip
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +34,12 @@ class CharmDeploymentConfiguration:
 
 ISTIO_K8S = CharmDeploymentConfiguration(
     entity_url="istio-k8s", application_name="istio-k8s", channel="latest/edge", trust=True
+)
+ISTIO_INGRESS_K8S = CharmDeploymentConfiguration(
+    entity_url="istio-ingress-k8s",
+    application_name="istio-ingress-k8s",
+    channel="latest/edge",
+    trust=True,
 )
 PROMETHEUS_K8S = CharmDeploymentConfiguration(
     entity_url="prometheus-k8s",
@@ -62,6 +68,7 @@ async def test_deploy_dependencies(ops_test: OpsTest):
     """Deploy the integration test dependencies."""
     await ops_test.model.deploy(**asdict(ISTIO_K8S))
     await ops_test.model.deploy(**asdict(PROMETHEUS_K8S))
+    await ops_test.model.deploy(**asdict(ISTIO_INGRESS_K8S))
 
     await ops_test.model.add_relation(ISTIO_K8S.application_name, PROMETHEUS_K8S.application_name)
 
@@ -87,11 +94,39 @@ async def test_add_relation_prometheus(ops_test: OpsTest):
 async def test_kiali_is_available(ops_test: OpsTest):
     """Assert that Kiali is up and available inside the cluster."""
     # Arrange - get the Kiali service IP
-    kiali_service_ip = get_k8s_service_clusterip(ops_test.model.name, APP_NAME)
+    kiali_service_ip = get_k8s_service_ip(ops_test.model.name, APP_NAME)
 
     # Assert that Kiali is available via the charm's service
     assert kiali_service_ip is not None, "Kiali service IP not found"
-    resp = requests.get(url=f"http://{kiali_service_ip}:20001/{ops_test.model.name}-{APP_NAME}")
+    resp = requests.get(url=f"http://{kiali_service_ip}:20001/kiali")
+    assert resp.status_code == 200, "Kiali is not available"
+
+
+@pytest.mark.setup
+@pytest.mark.abort_on_fail
+async def test_ingress_relation(ops_test: OpsTest):
+    """Relate kiali to istio-ingress."""
+    await ops_test.model.add_relation(ISTIO_INGRESS_K8S.application_name, APP_NAME)
+
+    await ops_test.model.wait_for_idle(
+        apps=[ISTIO_INGRESS_K8S.application_name, APP_NAME],
+        status="active",
+        raise_on_blocked=True,
+        timeout=1000,
+    )
+
+
+@pytest.mark.abort_on_fail
+async def test_ingress_is_available(ops_test: OpsTest):
+    """Assert that Kiali is exposed correctly and available via the ingress url."""
+    # Arrange - get the Ingress IP
+    ingress_ip = get_k8s_service_ip(
+        ops_test.model.name, f"{ISTIO_INGRESS_K8S.application_name}-lb"
+    )
+
+    # Assert that Kiali is available via the ingress service
+    assert ingress_ip is not None, "Ingress IP not found"
+    resp = requests.get(url=f"http://{ingress_ip}/{ops_test.model.name}-{APP_NAME}")
     assert resp.status_code == 200, "Kiali is not available"
 
 
