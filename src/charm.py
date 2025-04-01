5495b21c996e048265c55003e7ee6bbc85359462
Andrew Scribner <ca.scribner+1@gmail.com>
2025-04-01 16:08:48 -0400

    feat: integrate grafana (#44)
    * feat: add grafana integration
    
    Adds integration to grafana using the grafana-metadata interface.
    
    Not included here is setting up authentication between Kiali and Grafana.  See https://github.com/canonical/kiali-k8s-operator/issues/43 for more details.  This means that while the integration exists, it may not provide all functionality unless related to an unauthenticated Grafana.

--- src/charm.py
+++ src/charm.py
@@ -7,12 +7,13 @@
 
 import logging
 from pathlib import Path
-from typing import Optional
+from typing import Optional, TypedDict
 from urllib.parse import urlparse
 
 import ops
 import requests
 import yaml
+from charms.grafana_k8s.v0.grafana_metadata import GrafanaMetadataRequirer
 from charms.istio_beacon_k8s.v0.service_mesh import ServiceMeshConsumer
 from charms.istio_k8s.v0.istio_metadata import IstioMetadataRequirer
 from charms.loki_k8s.v1.loki_push_api import LogForwarder
@@ -29,6 +30,7 @@ from workload_config import (
     AuthConfig,
     DeploymentConfig,
     ExternalServicesConfig,
+    GrafanaConfig,
     KialiConfigSpec,
     PrometheusConfig,
     ServerConfig,
@@ -43,6 +45,14 @@ KIALI_PORT = 20001
 KIALI_PEBBLE_SERVICE_NAME = "kiali"
 ISTIO_RELATION = "istio-metadata"
 PROMETHEUS_RELATION = "prometheus"
+GRAFANA_RELATION = "grafana-metadata"
+
+
+class GrafanaUrls(TypedDict):
+    """A dictionary of Grafana URLs."""
+
+    internal_url: str
+    external_url: str
 
 
 class KialiCharm(ops.CharmBase):
@@ -91,8 +101,16 @@ class KialiCharm(ops.CharmBase):
         # Expose the Kiali workload through the service
         self.unit.set_ports(Port("tcp", KIALI_PORT))
 
+        # Connection to the Grafana used for deep dives into the metrics from Kiali
+        self._grafana_metadata = GrafanaMetadataRequirer(
+            relation_mapping=self.model.relations, relation_name=GRAFANA_RELATION
+        )
+        self.framework.observe(self.on[GRAFANA_RELATION].relation_changed, self.reconcile)
+        self.framework.observe(self.on[GRAFANA_RELATION].relation_broken, self.reconcile)
+
     def reconcile(self, _event: ops.ConfigChangedEvent):
         """Reconcile the entire state of the charm."""
+        LOGGER.debug("Reconciling Kiali charm")
         status_manager = StatusManager()
 
         # Set a default value for any returns in the below context in case of an error
@@ -104,10 +122,25 @@ class KialiCharm(ops.CharmBase):
         with status_manager:
             istio_namespace = self._get_istio_namespace()
 
+        with status_manager:
+            try:
+                grafana_urls = self._get_grafana_urls()
+                grafana_internal_url = grafana_urls["internal_url"]
+                grafana_external_url = grafana_urls["external_url"]
+            except GrafanaMissingError:
+                # Grafana integration is optional for the charm.  If the relation is Blocked (eg: does not exist) the
+                # charm will log and ignore it.  But if the relation is Waiting, we catch the status normally.
+                grafana_internal_url = None
+                grafana_external_url = None
+                LOGGER.info("Grafana integration disabled - no grafana relation found.")
+
         kiali_config = None
         with status_manager:
             kiali_config = self._generate_kiali_config(
-                prometheus_url=prometheus_url, istio_namespace=istio_namespace
+                prometheus_url=prometheus_url,
+                istio_namespace=istio_namespace,
+                grafana_internal_url=grafana_internal_url,
+                grafana_external_url=grafana_external_url,
             )
 
         with status_manager:
@@ -132,12 +165,14 @@ class KialiCharm(ops.CharmBase):
         Args:
             new_config: The new configuration to push to the Kiali workload.
         """
+        LOGGER.debug("Configuring Kiali workload")
         name = "kiali"
         if not self._container.can_connect():
             LOGGER.debug(f"Container is not ready, cannot configure {name}")
             raise WaitingStatusError("Container is not ready, cannot configure Kiali")
 
         if not new_config:
+            LOGGER.debug("No new_config provided.  raising Blocked StatusError.")
             raise BlockedStatusError("No configuration available for Kiali")
 
         layer = self._generate_kiali_layer()
@@ -155,9 +190,14 @@ class KialiCharm(ops.CharmBase):
             self._container.restart(KIALI_PEBBLE_SERVICE_NAME)
 
     def _generate_kiali_config(
-        self, prometheus_url: Optional[str], istio_namespace: Optional[str]
+        self,
+        prometheus_url: Optional[str],
+        istio_namespace: Optional[str],
+        grafana_internal_url: Optional[str],
+        grafana_external_url: Optional[str],
     ) -> dict:
         """Generate the Kiali configuration."""
+        LOGGER.debug("Generating Kiali configuration")
         if not prometheus_url:
             raise BlockedStatusError("Cannot configure Kiali - no Prometheus url available")
 
@@ -183,13 +223,18 @@ class KialiCharm(ops.CharmBase):
         # ),
         # )
 
-        # TODO:implement _get_grafana_source_url()
-        # grafana_url = self._get_grafana_source_url()
-        # if grafana_url:
-        # external_services.grafana = GrafanaConfig(
-        #     enabled=True,
-        #     external_url=grafana_url,
-        # )
+        if grafana_internal_url and grafana_external_url:
+            LOGGER.info(
+                "Grafana integration only works when connected to unauthenticated grafana instances."
+            )
+            # Kiali doesn't accept trailing slashes on grafana urls
+            grafana_internal_url = grafana_internal_url.rstrip("/")
+            grafana_external_url = grafana_external_url.rstrip("/")
+            external_services.grafana = GrafanaConfig(
+                enabled=True,
+                internal_url=grafana_internal_url,
+                external_url=grafana_external_url,
+            )
 
         kiali_config = KialiConfigSpec(
             auth=AuthConfig(strategy="anonymous"),
@@ -198,13 +243,17 @@ class KialiCharm(ops.CharmBase):
             istio_namespace=istio_namespace,
             server=ServerConfig(port=KIALI_PORT, web_root=self._prefix),
         )
-        return kiali_config.model_dump(exclude_none=True)
+
+        returned = kiali_config.model_dump(exclude_none=True)
+        LOGGER.debug(f"Kiali configuration: {returned}")
+        return returned
 
     @staticmethod
     def _generate_kiali_layer() -> Layer:
         """Generate the Kiali layer."""
         # TODO: Add pebble checks?
-        return Layer(
+        LOGGER.debug("Generating Kiali layer")
+        layer = Layer(
             {
                 "summary": "Kiali",
                 "description": "The Kiali dashboard for Istio",
@@ -219,6 +268,37 @@ class KialiCharm(ops.CharmBase):
                 },
             }
         )
+        LOGGER.debug(f"Kiali layer: {layer}")
+        return layer
+
+    def _get_grafana_urls(self) -> GrafanaUrls:
+        """Return the urls for the related Grafana.
+
+        This retrieves the grafana urls from the grafana-metadata relation, returning:
+        * internal_url: GrafanaMetadataAppData.direct_url
+        * external_url: GrafanaMetadataAppData.ingress_url
+
+        If GrafanaMetadataAppData.ingress_url is not available, it will default to GrafanaMetadataAppData.direct_url.
+
+        Raises:
+          GrafanaMissingError: If no grafana is related to this application
+          WaitingStatusError: If a grafana is related to this application, but its data is incomplete.
+        """
+        LOGGER.debug("Getting Grafana configuration")
+        if len(self._grafana_metadata.relations) == 0:
+            raise GrafanaMissingError("No grafana available over the grafana-metadata relation")
+
+        grafana_metadata = self._grafana_metadata.get_data()
+        if not grafana_metadata:
+            raise WaitingStatusError("Waiting on data over the grafana-metadata relation")
+
+        urls = GrafanaUrls(
+            internal_url=str(grafana_metadata.direct_url),
+            external_url=str(grafana_metadata.ingress_url or grafana_metadata.direct_url),
+        )
+
+        LOGGER.debug(f"Grafana urls: {urls}")
+        return urls
 
     def _get_istio_namespace(self) -> str:
         """Get the istio namespace configuration.
@@ -227,11 +307,14 @@ class KialiCharm(ops.CharmBase):
             BlockedStatusError: If no istio relation is available
             WaitingStatusError: If the istio relation is available, but the data is incomplete
         """
+        LOGGER.debug("Getting Istio namespace")
         if len(self._istio_metadata.relations) == 0:
             raise BlockedStatusError("Missing required relation to istio provider")
         if not (istio_data := self._istio_metadata.get_data()):
             raise WaitingStatusError("Istio relation established, but data is missing or invalid")
-        return istio_data.root_namespace
+        returned = istio_data.root_namespace
+        LOGGER.debug(f"Istio namespace: {returned}")
+        return returned
 
     def _get_prometheus_source_url(self) -> str:
         """Get the Prometheus source configuration.
@@ -244,6 +327,7 @@ class KialiCharm(ops.CharmBase):
             BlockedStatusError: If no Prometheus sources are available
             WaitingStatusError: If Prometheus sources are available, but the data is incomplete
         """
+        LOGGER.debug("Getting Prometheus source URL")
         if len(self._prometheus_source.relations) == 0:
             raise BlockedStatusError("Missing required relation to prometheus provider")
         if not (prometheus_data := self._prometheus_source.get_data()):
@@ -251,7 +335,9 @@ class KialiCharm(ops.CharmBase):
                 "Prometheus relation established, but data is missing or invalid"
             )
         # Return ingress_url if not None, else direct_url
-        return str(prometheus_data.ingress_url or prometheus_data.direct_url)
+        returned = str(prometheus_data.ingress_url or prometheus_data.direct_url)
+        LOGGER.debug(f"Prometheus source URL: {returned}")
+        return returned
 
     def _is_prometheus_source_available(self):
         """Return True if Prometheus is available, else False."""
@@ -290,7 +376,9 @@ def _is_container_file_equal_to(container: Container, filename: str, file_conten
 
     Returns False if the container is not accessible, the file does not exist, or the contents do not match.
     """
+    LOGGER.debug(f"Checking if {filename} in container is equal to passed contents")
     if not container.can_connect():
+        LOGGER.debug(f"Container is not ready, cannot check {filename}")
         return False
 
     try:
@@ -299,7 +387,9 @@ def _is_container_file_equal_to(container: Container, filename: str, file_conten
         LOGGER.warning(f"Could not check {filename} - got error while retrieving the file: {e}")
         return False
 
-    return current_contents == file_contents
+    returned = current_contents == file_contents
+    LOGGER.debug(f"Result of current_contents == file_contents: {returned}")
+    return returned
 
 
 def _is_kiali_available(kiali_url):
@@ -324,6 +414,12 @@ def _is_kiali_available(kiali_url):
 
 class PrometheusSourceError(Exception):
     """Raised when the Prometheus data is not available."""
+
+    pass
+
+
+class GrafanaMissingError(Exception):
+    """Raised when the Grafana data is not available."""
 
     pass
 
--- tests/scenario/test_charm.py
+++ tests/scenario/test_charm.py
@@ -3,42 +3,62 @@
 # See LICENSE file for licensing details.
 from contextlib import nullcontext as does_not_raise
 from typing import Optional
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 from observability_charm_tools.exceptions import BlockedStatusError, WaitingStatusError
 from ops import ActiveStatus, BlockedStatus, WaitingStatus
 from scenario import Container, Relation, State
 
-REMOTE_PROMETHEUS_APP_NAME = "grafana"
+from charm import KialiCharm
+
 REMOTE_PROMETHEUS_MODEL = "some-model"
 REMOTE_PROMETHEUS_MODEL_UUID = "1"
 REMOTE_PROMETHEUS_TYPE = "prometheus"
-REMOTE_PROMETHEUS_URL = "http://prometheus:9090"
+REMOTE_PROMETHEUS_URL = "http://prometheus:9090/"
 REMOTE_ISTIO_APP_NAME = "istio"
 REMOTE_ISTIO_NAMESPACE = "istio-model"
+GRAFANA_INTERNAL_URL = "http://grafana:3000/"
+GRAFANA_EXTERNAL_URL = "http://grafana.example.com/"
+GRAFANA_UID = "grafana-uid"
 
 
-def mock_prometheus_relation() -> Relation:
-    """Return a mock relation to prometheus."""
+def mock_grafana_relation(
+    internal_url=GRAFANA_INTERNAL_URL, external_url=GRAFANA_EXTERNAL_URL
+) -> Relation:
+    """Return a mock relation to grafana."""
     return Relation(
-        endpoint="prometheus",
-        interface="prometheus_api",
-        remote_app_name=REMOTE_PROMETHEUS_APP_NAME,
+        endpoint="grafana-metadata",
+        interface="grafana_metadata",
+        remote_app_name="grafana",
         remote_app_data={
-            "direct_url": REMOTE_PROMETHEUS_URL,
+            "direct_url": internal_url,
+            "ingress_url": external_url,
+            "grafana_uid": GRAFANA_UID,
         },
     )
 
 
-def mock_istio_metadata_relation() -> Relation:
+def mock_prometheus_relation(direct_url=REMOTE_PROMETHEUS_URL) -> Relation:
+    """Return a mock relation to prometheus."""
+    return Relation(
+        endpoint="prometheus",
+        interface="prometheus_api",
+        remote_app_name="prometheus",
+        remote_app_data={
+            "direct_url": direct_url,
+        },
+    )
+
+
+def mock_istio_metadata_relation(root_namespace=REMOTE_ISTIO_NAMESPACE) -> Relation:
     """Return a mock relation for istio-metadata."""
     return Relation(
         endpoint="istio-metadata",
         interface="istio_metadata",
-        remote_app_name=REMOTE_ISTIO_APP_NAME,
+        remote_app_name="istio",
         remote_app_data={
-            "root_namespace": REMOTE_ISTIO_NAMESPACE,
+            "root_namespace": root_namespace,
         },
     )
 
@@ -58,7 +78,7 @@ def mock_is_kiali_available(raises: Optional[Exception]):
     "container, relations, kiali_available_mock, expected_status",
     [
         (
-            # Has all inputs - Active
+            # Has prometheus and istio-metadata - Active
             Container(name="kiali", can_connect=True),
             [
                 mock_prometheus_relation(),
@@ -125,17 +145,45 @@ def test_charm_given_inputs(
 
 
 @pytest.mark.parametrize(
-    "prometheus_url, istio_namespace, expected, expected_context",
+    "prometheus_url, istio_namespace, grafana_internal_url, grafana_external_url, expected, expected_context",
     [
         (
             # Active: All inputs provided.
-            "http://prometheus:9090",
-            "istio-namespace",
+            REMOTE_PROMETHEUS_URL,
+            REMOTE_ISTIO_NAMESPACE,
+            # Include a trailing slash here to ensure we remove them during parsing.  Kiali doesn't accept trailing
+            # slashes
+            GRAFANA_INTERNAL_URL,
+            GRAFANA_EXTERNAL_URL,
             {
                 "auth": {"strategy": "anonymous"},
                 "deployment": {"view_only_mode": True},
-                "external_services": {"prometheus": {"url": REMOTE_PROMETHEUS_URL}},
-                "istio_namespace": "istio-namespace",
+                "external_services": {
+                    "prometheus": {"url": REMOTE_PROMETHEUS_URL},
+                    "grafana": {
+                        "enabled": True,
+                        "internal_url": GRAFANA_INTERNAL_URL.rstrip("/"),
+                        "external_url": GRAFANA_EXTERNAL_URL.rstrip("/"),
+                    },
+                },
+                "istio_namespace": REMOTE_ISTIO_NAMESPACE,
+                "server": {"port": 20001, "web_root": "/"},
+            },
+            does_not_raise(),
+        ),
+        (
+            # Active: All inputs except optional grafana provided.
+            REMOTE_PROMETHEUS_URL,
+            REMOTE_ISTIO_NAMESPACE,
+            None,
+            None,
+            {
+                "auth": {"strategy": "anonymous"},
+                "deployment": {"view_only_mode": True},
+                "external_services": {
+                    "prometheus": {"url": REMOTE_PROMETHEUS_URL},
+                },
+                "istio_namespace": REMOTE_ISTIO_NAMESPACE,
                 "server": {"port": 20001, "web_root": "/"},
             },
             does_not_raise(),
@@ -145,11 +193,15 @@ def test_charm_given_inputs(
             None,
             "istio-namespace",
             None,
+            None,
+            None,
             pytest.raises(BlockedStatusError),
         ),
         (
             # Inactive: Missing istio namespace should raise an exception.
             "http://prometheus:9090",
+            None,
+            None,
             None,
             None,
             pytest.raises(BlockedStatusError),
@@ -161,6 +213,8 @@ def test_kiali_config(
     this_charm_context,
     prometheus_url,
     istio_namespace,
+    grafana_internal_url,
+    grafana_external_url,
     expected,
     expected_context,
 ):
@@ -170,7 +224,76 @@ def test_kiali_config(
         # Default value in case we raise an exception
         with expected_context:
             kiali_config = charm._generate_kiali_config(
-                prometheus_url=prometheus_url, istio_namespace=istio_namespace
+                prometheus_url=prometheus_url,
+                istio_namespace=istio_namespace,
+                grafana_internal_url=grafana_internal_url,
+                grafana_external_url=grafana_external_url,
             )
             # If above doesn't raise, compare output
             assert kiali_config == expected
+
+
+@pytest.mark.parametrize(
+    "prometheus_relation, istio_metadata_relation, grafana_metadata_relation",
+    [
+        (
+            mock_prometheus_relation(direct_url=REMOTE_PROMETHEUS_URL),
+            mock_istio_metadata_relation(root_namespace=REMOTE_ISTIO_NAMESPACE),
+            None,
+        ),
+        (
+            mock_prometheus_relation(direct_url=REMOTE_PROMETHEUS_URL),
+            mock_istio_metadata_relation(root_namespace=REMOTE_ISTIO_NAMESPACE),
+            mock_grafana_relation(
+                internal_url=GRAFANA_INTERNAL_URL, external_url=GRAFANA_EXTERNAL_URL
+            ),
+        ),
+    ],
+)
+def test_e2e_charm_configuration(
+    this_charm_context, prometheus_relation, istio_metadata_relation, grafana_metadata_relation
+):
+    """An end-to-end spot test confirming configuration is correctly passed from relations to _generate_kiali_config."""
+    # Arrange
+    relations = []
+    prometheus_url_expected = None
+    istio_namespace_expected = None
+    grafana_internal_url_expected = None
+    grafana_external_url_expected = None
+
+    if prometheus_relation:
+        relations.append(prometheus_relation)
+        prometheus_url_expected = prometheus_relation.remote_app_data["direct_url"]
+    if istio_metadata_relation:
+        relations.append(istio_metadata_relation)
+        istio_namespace_expected = istio_metadata_relation.remote_app_data["root_namespace"]
+    if grafana_metadata_relation:
+        relations.append(grafana_metadata_relation)
+        # remove the trailing slash, as we intentionally strip it out to keep Kiali happy
+        grafana_internal_url_expected = grafana_metadata_relation.remote_app_data["direct_url"]
+        grafana_external_url_expected = grafana_metadata_relation.remote_app_data["ingress_url"]
+
+    state = State(
+        containers=[
+            Container(name="kiali", can_connect=True),
+        ],
+        relations=relations,
+        leader=True,
+    )
+
+    # Act
+    with this_charm_context(this_charm_context.on.config_changed(), state) as manager:
+        charm: KialiCharm = manager.charm
+        mock_generate_kiali_config = MagicMock()
+        charm._generate_kiali_config = mock_generate_kiali_config
+        # We don't need to actually configure anything
+        charm._configure_kiali_workload = MagicMock()
+        manager.run()
+
+        # Assert
+        mock_generate_kiali_config.assert_called_once_with(
+            prometheus_url=prometheus_url_expected,
+            istio_namespace=istio_namespace_expected,
+            grafana_internal_url=grafana_internal_url_expected,
+            grafana_external_url=grafana_external_url_expected,
+        )
